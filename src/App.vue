1b4ef46fce358a64148b0805544c312d95d7e37b
Mevlüt Canvar <mevlutcanvar@gmail.com>
2020-12-01 02:43:09 +0300

    adds js sdk and implements network status

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Algorand network status
+      <a href="#" @click.prevent="fetchStatus"> <refresh-cw-icon size="0.5x"></refresh-cw-icon></a>
+    </h1>
+    <vue-json-pretty
+        :data="status">
+    </vue-json-pretty>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueJsonPretty from 'vue-json-pretty'
+import { RefreshCwIcon } from 'vue-feather-icons'
+import 'vue-json-pretty/lib/styles.css'
 export default {
   name: 'App',
+  data: () => {
+    return {
+      client: null,
+      token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      server: 'http://localhost',
+      port: 4001,
+      status: {}
+    };
+  },
   components: {
-    HelloWorld
+    VueJsonPretty,
+    RefreshCwIcon
+  },
+  methods: {
+    fetchStatus() {
+      this.client.status()
+          .do()
+          .then(data => this.status = data);
+    }
+  },
+  created() {
+    this.client = new window.algosdk.Algodv2(this.token, this.server, this.port);
+
+    this.fetchStatus();
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2c3e50;
+  color: azure;
+  margin-top: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './core/algosdk.min'
 
 Vue.config.productionTip = false
 
