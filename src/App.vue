384d1646c2f2b53a831ea46a351f68ceab0ef1e4
Mevlüt Canvar <mevlutcanvar@gmail.com>
2020-12-04 03:36:49 +0300

    implements sending pay transaction

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <h1>
       Algorand network status
-      <a href="#" @click.prevent="fetchStatus"> <refresh-cw-icon size="0.5x"></refresh-cw-icon></a>
+      <a href="#" @click.prevent="fetchStatus">
+        <refresh-cw-icon size="0.5x"></refresh-cw-icon>
+      </a>
     </h1>
     <vue-json-pretty
         :data="status">
@@ -12,7 +14,9 @@
 
     <h1>
       Algorand suggested parameters
-      <a href="#" @click.prevent="fetchParams"> <refresh-cw-icon size="0.5x"></refresh-cw-icon></a>
+      <a href="#" @click.prevent="fetchParams">
+        <refresh-cw-icon size="0.5x"></refresh-cw-icon>
+      </a>
     </h1>
     <vue-json-pretty
         :data="params">
@@ -29,22 +33,43 @@
       Fund the account for testnet
     </a>
     <a href="#" @click.prevent="copyAddress" class="link">
-      <copy-icon class="link-icon" size="1.0x"></copy-icon> {{ copyAddressText }}
+      <copy-icon class="link-icon" size="1.0x"></copy-icon>
+      {{ copyAddressText }}
     </a>
     <br>
-    <vue-json-pretty
-        :data="account">
-    </vue-json-pretty>
+    <h2>Address: <span>{{ account.address }}</span></h2>
 
     <hr>
 
     <h1>
       Account Info
-      <a href="#" @click.prevent="fetchAccountInfo"> <refresh-cw-icon size="0.5x"></refresh-cw-icon></a>
+      <a href="#" @click.prevent="fetchAccountInfo">
+        <refresh-cw-icon size="0.5x"></refresh-cw-icon>
+      </a>
     </h1>
     <vue-json-pretty
         :data="accountInfo">
     </vue-json-pretty>
+
+    <hr>
+
+    <h1>
+      Send a Pay Transaction To TestNet Faucet Address
+    </h1>
+    <input type="number" min="0" v-model="amount" :disabled="sending">
+    <button :disabled="!validAmount" @click="submitTxt">
+      Send {{ this.amount > 0 ? this.amount : '' }} Algos
+    </button>
+    <br>
+    <ol>
+      <li v-for="(step, index) in txSteps" :key="index">{{ step }}</li>
+    </ol>
+    <a v-if="txId" :href="explorerLink" target="_blank" class="link link-left">
+      <external-link-icon class="link-icon" size="1.0x"></external-link-icon>
+      Show on Algorand BC Explorer for TestNet
+    </a>
+
+    <br>
 
   </div>
 </template>
@@ -52,12 +77,19 @@
 <script>
 import localforage from 'localforage'
 import VueJsonPretty from 'vue-json-pretty'
-import { RefreshCwIcon, CopyIcon, ExternalLinkIcon } from 'vue-feather-icons'
+import {RefreshCwIcon, CopyIcon, ExternalLinkIcon} from 'vue-feather-icons'
 import 'vue-json-pretty/lib/styles.css'
+
 export default {
   name: 'App',
   data: () => {
     return {
+      TESTNET_FAUCET_ADDRESS: 'GD64YIY3TWGDMCNPP553DZPPR6LDUSFQOIJVFDPPXWEG3FVOJCCDBBHU5A',
+      TXN_FEE: 1000,
+      TXN_NOTE: 'Generated from git@github.com:crx4/algorand-sandbox-examples.git',
+      amount: '0',
+      txId: '',
+      txSteps: [],
       client: null,
       token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
       server: 'http://localhost',
@@ -66,7 +98,8 @@ export default {
       params: {},
       account: {},
       accountInfo: {},
-      copyAddressText: 'Copy address'
+      copyAddressText: 'Copy address',
+      sending: false
     };
   },
   components: {
@@ -78,6 +111,14 @@ export default {
   computed: {
     accountAddress() {
       return 'address' in this.account ? this.account.address : '';
+    },
+    validAmount() {
+      return parseInt(this.amount) > 0 &&
+          parseInt(this.amount + '000000') <= (parseInt(this.accountInfo.amount) - this.TXN_FEE) &&
+          !this.sending;
+    },
+    explorerLink() {
+      return 'https://testnet.algoexplorer.io/tx/' + this.txId;
     }
   },
   methods: {
@@ -87,15 +128,19 @@ export default {
           .then(data => this.status = data);
     },
     fetchParams() {
-      this.client.getTransactionParams()
+      return this.client.getTransactionParams()
           .do()
           .then(data => this.params = data);
     },
     createNewAccount(recreate = false) {
       let account = window.algosdk.generateAccount();
+
       this.account = {
         address: account.addr,
-        passphrase: window.algosdk.secretKeyToMnemonic(account.sk)
+        detail: {
+          sk: account.sk,
+          passphrase: window.algosdk.secretKeyToMnemonic(account.sk)
+        }
       };
 
       if (recreate) {
@@ -136,6 +181,53 @@ export default {
       this.copyAddressText = 'Copied!';
 
       setTimeout(() => this.copyAddressText = 'Copy Address', 1000);
+    },
+    async submitTxt() {
+      this.txSteps = [];
+      this.sending = true;
+
+      this.txSteps.push('Fetching actual network parameters.');
+      await this.fetchParams();
+
+      this.params.fee = this.TXN_FEE;
+      this.params.flatFee = true;
+
+      let txn = window.algosdk.makePaymentTxnWithSuggestedParams(
+          this.account.address,
+          this.TESTNET_FAUCET_ADDRESS,
+          parseInt(this.amount + '000000'),
+          undefined,
+          window.algosdk.encodeObj(this.TXN_NOTE),
+          this.params
+      );
+
+      this.txSteps.push('Signing transaction.');
+      let signedTxn = txn.signTxn(this.account.detail.sk);
+      this.txId = txn.txID().toString();
+
+      this.txSteps.push('Submitting transaction to the TestNet. TxID: ' + this.txId);
+      await this.client.sendRawTransaction(signedTxn).do();
+      this.txSteps.push('Transaction submitted!');
+
+      await this.waitForConfirmation();
+
+      this.fetchAccountInfo();
+
+      this.sending = false;
+    },
+    async waitForConfirmation() {
+      let status = (await this.client.status().do());
+      let lastRound = status["last-round"];
+      while (this.sending) {
+        const pendingInfo = await this.client.pendingTransactionInformation(this.txId).do();
+        if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+          this.txSteps.push("Transaction " + this.txId + " confirmed in round " + pendingInfo["confirmed-round"]);
+          break;
+        }
+        this.txSteps.push('Waiting for confirmation.');
+        lastRound++;
+        await this.client.statusAfterBlock(lastRound).do();
+      }
     }
   },
   created() {
@@ -168,12 +260,26 @@ export default {
     color: wheat;
   }
 
+  ol {
+    color: #13ce66;
+  }
+
+  h2 span {
+    color: #13ce66;
+  }
+
   .link {
     margin-bottom: 1rem;
     float: right;
     font-size: small;
 
-    .link-icon { margin-left: 2rem; }
+    .link-icon {
+      margin-left: 2rem;
+    }
+  }
+
+  .link-left {
+    float: left !important;
   }
 
   h1 {
